493bdea94e467395af07ad33d63f26945b87ffe2
tania <taniaosano15@gmail.com>
2022-08-30 10:53:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+  for(let i = 0; i < array.length; i++){
+    
+    const x = target - array[i];
+    for(let y = i+1; y < array.length; y++){
+    if(array[y] === x)
+      return true;
+    }
+  }
+  return false;
 }
-
+hasTargetSum([3,8,6,4,1,9], 10);
 /* 
   Write the Big O time complexity of your function here
+
+  Given an input array of n elements, worst case scenario is that the algorithm needs
+  make (n*n) iterations
 */
 
 /* 
   Add your pseudocode here
+
+  Iterate through each number in the array
+  loop through the rest of the array
+  If sum of two numbers in the array match with the target, return true
+  else return false
 */
 
 /*
